8e59a7ff8e824df1813b3d93c2ee10fc489a71a7
ocampov98 <53489401+ocampov98@users.noreply.github.com>
2020-06-07 02:07:33 -0600

    First Commit

---
+++ src/main/java/org/example/basicapp/App.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.example.basicapp;
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        DatabaseAccess dbAccess = new DatabaseAccess();
+
+        //dbAccess.createTable("Movies");
+
+        //dbAccess.loadDataFromJSONFile("Movies","moviedata.json");
+
+        //dbAccess.addNewItem("Movies",2015,"The Big New Movie","Nothing happens at all.",0);
+
+        //dbAccess.readItem("Movies",2015,"The Big New Movie");
+
+        //String[] actors = {"Larry", "Moe", "Curly"};
+        //dbAccess.updateItem("Movies",2015,"The Big New Movie",5.5,"Everything happens all at once.", actors);
+
+        //dbAccess.conditionallyUpdateItem("Movies",2015,"The Big New Movie");
+
+        //dbAccess.deleteItem("Movies",2015,"The Big New Movie");
+
+        //dbAccess.itemsQuery("Movies",1985,"A","L");
+
+        //dbAccess.scanTable("Movies",1950,1959);
+
+        //dbAccess.deleteTable("Movies");
+    }
+
+
+}
+
---
+++ src/main/java/org/example/basicapp/DatabaseAccess.java
@@ -0,0 +1,288 @@
+package org.example.basicapp;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.*;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class DatabaseAccess {
+
+    AmazonDynamoDB client;
+    DynamoDB dynamoDB;
+
+    public DatabaseAccess(){
+        client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_WEST_2)
+                .build();
+
+        dynamoDB = new DynamoDB(client);
+    }
+
+    public void createTable(String tableName){
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
+                            // key
+                            new KeySchemaElement("title", KeyType.RANGE)), // Sort key
+                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                            new AttributeDefinition("title", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public void deleteTable(String tableName){
+        Table table = dynamoDB.getTable(tableName);
+
+        try {
+            System.out.println("Attempting to delete table; please wait...");
+            table.delete();
+            table.waitForDelete();
+            System.out.print("Success.");
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to delete table: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void loadDataFromJSONFile(String tableName, String filePath) throws Exception{
+        Table table = dynamoDB.getTable(tableName);
+
+        JsonParser parser = new JsonFactory().createParser(new File(filePath));
+
+        JsonNode rootNode = new ObjectMapper().readTree(parser);
+        Iterator<JsonNode> iter = rootNode.iterator();
+
+        ObjectNode currentNode;
+
+        while (iter.hasNext()) {
+            currentNode = (ObjectNode) iter.next();
+
+            int year = currentNode.path("year").asInt();
+            String title = currentNode.path("title").asText();
+
+            try {
+                table.putItem(new Item().withPrimaryKey("year", year, "title", title).withJSON("info",
+                        currentNode.path("info").toString()));
+                System.out.println("PutItem succeeded: " + year + " " + title);
+
+            }
+            catch (Exception e) {
+                System.err.println("Unable to add movie: " + year + " " + title);
+                System.err.println(e.getMessage());
+                break;
+            }
+        }
+        parser.close();
+    }
+
+    public void addNewItem(String tableName, int year, String title, String plot, int rating){
+        Table table = dynamoDB.getTable(tableName);
+
+        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        infoMap.put("plot", plot);
+        infoMap.put("rating", rating);
+
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table
+                    .putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
+
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void readItem(String tableName, int year, String title){
+        Table table = dynamoDB.getTable(tableName);
+
+        GetItemSpec spec = new GetItemSpec().withPrimaryKey("year", year, "title", title);
+
+        try {
+            System.out.println("Attempting to read the item...");
+            Item outcome = table.getItem(spec);
+            System.out.println("GetItem succeeded: " + outcome);
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to read item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public void updateItem(String tableName, int year, String title, double rating, String plot, String[] actors){
+        Table table = dynamoDB.getTable(tableName);
+
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("year", year, "title", title)
+                .withUpdateExpression("set info.rating = :r, info.plot=:p, info.actors=:a")
+                .withValueMap(new ValueMap().withNumber(":r", rating).withString(":p", plot)
+                        .withList(":a", Arrays.asList(actors)))
+                .withReturnValues(ReturnValue.UPDATED_NEW);
+
+        try {
+            System.out.println("Updating the item...");
+            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+            System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to update item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void conditionallyUpdateItem(String tableName, int year, String title){
+        Table table = dynamoDB.getTable(tableName);
+
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                .withPrimaryKey(new PrimaryKey("year", year, "title", title)).withUpdateExpression("remove info.actors[0]")
+                .withConditionExpression("size(info.actors) >= :num").withValueMap(new ValueMap().withNumber(":num", 3))
+                .withReturnValues(ReturnValue.UPDATED_NEW);
+
+        // Conditional update (we expect this to fail)
+        try {
+            System.out.println("Attempting a conditional update...");
+            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+            System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to update item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void deleteItem(String tableName, int year, String title){
+        Table table = dynamoDB.getTable(tableName);
+
+
+        DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
+                .withPrimaryKey(new PrimaryKey("year", year, "title", title));
+
+        // Conditional delete (we expect this to fail)
+
+        try {
+            System.out.println("Attempting a conditional delete...");
+            table.deleteItem(deleteItemSpec);
+            System.out.println("DeleteItem succeeded");
+        }
+        catch (Exception e) {
+            System.err.println("Unable to delete item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void itemsQuery(String tableName, int year, String firstLetter, String secondLetter){
+        Table table = dynamoDB.getTable(tableName);
+
+
+        HashMap<String, String> nameMap = new HashMap<String, String>();
+        nameMap.put("#yr", "year");
+
+        HashMap<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.put(":yyyy", year);
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("#yr = :yyyy").withNameMap(nameMap)
+                .withValueMap(valueMap);
+
+        ItemCollection<QueryOutcome> items = null;
+        Iterator<Item> iterator = null;
+        Item item = null;
+
+        try {
+            System.out.println("Movies from "+year);
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getNumber("year") + ": " + item.getString("title"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query movies from "+year);
+            System.err.println(e.getMessage());
+        }
+
+        valueMap.put(":yyyy", year);
+        valueMap.put(":letter1", firstLetter);
+        valueMap.put(":letter2", secondLetter);
+
+        querySpec.withProjectionExpression("#yr, title, info.genres, info.actors[0]")
+                .withKeyConditionExpression("#yr = :yyyy and title between :letter1 and :letter2").withNameMap(nameMap)
+                .withValueMap(valueMap);
+
+        try {
+            System.out.println("Movies from " +year+" -titles " + firstLetter + "-"+ secondLetter + ", with genres and lead actor");
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getNumber("year") + ": " + item.getString("title") + " " + item.getMap("info"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query movies from " + year + ":");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void scanTable(String tableName, int startYear, int endYear){
+        Table table = dynamoDB.getTable(tableName);
+
+        ScanSpec scanSpec = new ScanSpec().withProjectionExpression("#yr, title, info.rating")
+                .withFilterExpression("#yr between :start_yr and :end_yr").withNameMap(new NameMap().with("#yr", "year"))
+                .withValueMap(new ValueMap().withNumber(":start_yr", startYear).withNumber(":end_yr", endYear));
+
+        try {
+            ItemCollection<ScanOutcome> items = table.scan(scanSpec);
+
+            Iterator<Item> iter = items.iterator();
+            while (iter.hasNext()) {
+                Item item = iter.next();
+                System.out.println(item.toString());
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to scan the table:");
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
